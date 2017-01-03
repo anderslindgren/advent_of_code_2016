e4ef394c4e343d8cca6ce3dd5d47cf36fadc8c7a
Anders Lindgren <anders.lindgren@klarna.com>
2017-01-03 23:41:54 +0100

    Day 20 - Part 1

---
+++ anderslindgren-java/day20/src/main/java/com/adventofcode/javatomten/Day20.java
@@ -0,0 +1,143 @@
+package com.adventofcode.javatomten;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+
+/**
+ * Advent of Code 2016-12-20.
+ */
+public class Day20 {
+
+    List<Range> blacklist = new ArrayList<>();
+
+    class Range implements Comparable {
+        long low;
+        long high;
+
+        Range(long low, long high) {
+            this.low = low;
+            this.high = high;
+        }
+
+        boolean isWithinRange(long value) {
+            return low < value && value < high;
+        }
+
+        void extendRange(Range range) {
+            if (range.low < this.low) {
+                this.low = range.low;
+            }
+            if (range.high > this.high) {
+                this.high = range.high;
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Range range = (Range) o;
+
+            return low == range.low && high == range.high;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = (int) (low ^ (low >>> 32));
+            result = 31 * result + (int) (high ^ (high >>> 32));
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + low + "-" + high + ']';
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            return Long.valueOf(low).compareTo(((Range) o).low);
+        }
+    }
+
+    public static void main(String... args) throws IOException, NoSuchAlgorithmException {
+        Day20 day = new Day20();
+        List<String> lines = day.parseArgs(args);
+        day.parse(lines);
+        day.findFirstWhite();
+    }
+
+    private List<String> parseArgs(String[] args) throws IOException {
+        Path path = FileSystems.getDefault().getPath(args[0]);
+        return Files.readAllLines(path);
+    }
+
+    private void parse(List<String> lines) {
+        lines.forEach(this::parse);
+    }
+
+    private void parse(String line) {
+        String[] range = line.split("-");
+        final long low = Long.parseLong(range[0]);
+        final long high = Long.parseLong(range[1]);
+
+        Range r = new Range(low, high);
+        blacklist.add(r);
+    }
+
+    private void findFirstWhite() {
+        List<Range> newBlacklist = compactBlacklist(blacklist);
+        //newBlacklist.forEach(System.out::println);
+        System.out.println(newBlacklist.get(0).high + 1);
+    }
+
+    private List<Range> compactBlacklist(List<Range> blacklist) {
+        List<Range> result = new ArrayList<>();
+        Collections.sort(blacklist);
+        for (int i = 0; i < blacklist.size(); i++) {
+            Range blackRange = blacklist.get(i);
+            if (result.size() == 0) {
+                result.add(blackRange);
+            }
+            else {
+                for (int j = 0; j < result.size(); j++) {
+                    Range resultRange = result.get(j);
+                    if (resultRange.isWithinRange(blackRange.low) && resultRange.isWithinRange(blackRange.high)) {
+                        blackRange = null;
+                        break;
+                    }
+                    else if (blackRange.low < resultRange.low && resultRange.isWithinRange(blackRange.high) ) {
+                        resultRange.extendRange(blackRange);
+                        blackRange = null;
+                        break;
+                    }
+                    else if (blackRange.high > resultRange.high && resultRange.isWithinRange(blackRange.low) ) {
+                        resultRange.extendRange(blackRange);
+                        blackRange = null;
+                        break;
+                    }
+                    else if (blackRange.low == resultRange.high + 1) {
+                        resultRange.extendRange(blackRange);
+                        blackRange = null;
+                        break;
+                    }
+                    else if (blackRange.high == resultRange.low - 1) {
+                        resultRange.extendRange(blackRange);
+                        blackRange = null;
+                        break;
+                    }
+                }
+                if (blackRange != null) {
+                    result.add(blackRange);
+                }
+            }
+        }
+        return result;
+    }
+
+}
